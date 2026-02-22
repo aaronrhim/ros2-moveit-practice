dbb2dc0b43852383cfa5c9422687e4ffaaa698ac
Aaron Rhim <rhimaaron@gmail.com>
2026-02-22 15:13:10 -0800

    testing publishers

--- aaron_ros2/include/moveit_control _2.h
+++ aaron_ros2/include/moveit_control _2.h
@@ -9,6 +9,18 @@
 #include "std_msgs/msg/string.hpp"
 
 #include "arm_constants_2.h"
+#include "rover_msgs/msg/arm_command.hpp"
+
+// path planning
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "control_msgs/msg/joint_trajectory_controller_state.hpp"
+#include "sensor_msgs/msg/joy.hpp"
+#include "trajectory_msgs/msg/joint_trajectory.hpp"
+
+// servo
+#include <moveit_servo/servo_parameters.h>
+#include <moveit_servo/servo.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
 
 #define CARTESIAN_EE_FRAME 2
 #define CARTESIAN_BASE_FRAME 3
@@ -23,10 +35,12 @@ class ArmMoveitControl2 : public rclcpp::Node {
             }
 
             // Rowan: auto qos = rclcpp::QoS(rclcpp::KeepLast(1)).reliable().durability_volatile();
-            // private publisher for the arm joints
-            arm_publisher_ = this->create_publisher<std_msgs::msg::String>("joint_states", 3);
-            // joint_cmd_publisher_ = this->create_publisher<control_msgs::msg::JointJog>(ArmConstants::servo_fk_topic, 10);
-            // twist_cmd_publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped>(ArmConstants::servo_ik_topic, 10);
+            // path planning
+            arm_publisher_ = this->create_publisher<rover_msgs::msg::ArmCommand>("joint_states", 3);
+
+            // servo
+            joint_cmd_publisher_ = this->create_publisher<control_msgs::msg::JointJog>(ArmConstants::servo_fk_topic, 10);
+            twist_cmd_publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped>(ArmConstants::servo_ik_topic, 10);
 
             // the node instantly prints to moveit path planning and moveit servo
             joy_subscriber = this->create_subscription<sensor_msgs::msg::Joy>(
@@ -51,7 +65,60 @@ class ArmMoveitControl2 : public rclcpp::Node {
         };
         JointStates joint_states[NUM_JOINTS];  // 6 axes on the arm + 1 for ee
 
-        void servoCallback(const trajectory_msgs::msg::JointTrajectory::SharedPtr msg) {            
+        /* Instantiation */
+        // path planning
+        rclcpp::Publisher<rover_msgs::msg::ArmCommand>::SharedPtr arm_publisher_;
+
+        // servo
+        rclcpp::Publisher<control_msgs::msg::JointJog>::SharedPtr joint_cmd_publisher_;
+        rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_cmd_publisher_;
+        rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_cmd_publisher_;
+
+        // post-servo-callback
+        rclcpp::Subscription<control_msgs::msg::JointTrajectoryControllerState>::SharedPtr trajectory_subscriber;
+        rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_subscriber;
+        rclcpp::Subscription<trajectory_msgs::msg::JointTrajectory>::SharedPtr servo_subscriber;
+
+        rclcpp::TimerBase::SharedPtr timer_;
+
+        void jointStateCallback(const sensor_msgs::msg::JointState::SharedPtr msg) {
+            RCLCPP_INFO(this->get_logger(), "Received joint state callback");
+            rover_msgs::msg::ArmCommand target;
+            target.positions.resize(NUM_JOINTS);
+            target.cmd_type = 'P';
+
+            for (int i = 0; i < NUM_JOINTS; i++) {
+                target.positions[i] = moveitToFirmwareOffset(msg->position[i], i);
+            }
+
+            arm_publisher_->publish(target);
+        }
+
+        void jointTrajectoryCallback(const control_msgs::msg::JointTrajectoryControllerState::SharedPtr msg) {
+            RCLCPP_INFO(this->get_logger(), "Received joint trajectory callback");
+            rover_msgs::msg::ArmCommand target;
+            target.velocities.resize(NUM_JOINTS_NO_EE);
+            target.cmd_type = 'V';
+
+            for (size_t i = 0; i < msg->output.velocities.size(); ++i) {
+                target.velocities[i] = moveitVelocityToFirmwareOffset(msg->output.velocities[i], i);
+            }
+            arm_publisher_->publish(target);
+        }
+
+        void arm_callback(const rover_msgs::msg::ArmCommand::SharedPtr msg) {
+            RCLCPP_INFO(this->get_logger(), "Received ArmCommand callback");
+            for (size_t i = 0; i < msg->positions.size(); ++i) {
+                RCLCPP_INFO(this->get_logger(), "Joint %zu: Position = %f", i, msg->positions[i]);
+            }
+        }
+
+        void servoCallback(const trajectory_msgs::msg::JointTrajectory::SharedPtr msg) {  
+            RCLCPP_INFO(this->get_logger(), "Received servo callback");
+            rover_msgs::msg::ArmCommand target;
+            target.velocities.resize(NUM_JOINTS_NO_EE);
+            target.cmd_type = 'V';
+            
             for (int i = 0; i < NUM_JOINTS_NO_EE; i++) {
                 joint_states[i].velocity = moveitVelocityToFirmwareOffset(msg->points[0].velocities[i], i);
             }
---
+++ aaron_ros2/notes.md
@@ -0,0 +1,11 @@
+# Ros2, Moveit2, and RViz2
+
+Looking at all three softwares form a system for robotic control. 
+
+Specialized controllers (system):
+- **Joint Trajectory Controller**: for arm movements
+- **Forward Velocity Controller**: for speed control
+- **Gripper Controller**: for end-effector actions
+- **Diff Drive Controller**: for movile base navigation
+
+
